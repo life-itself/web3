dac281993d3ac61b9c66c8bd2c4a7012b5f43d97
Rufus Pollock <rufus.pollock@datopian.com>
2022-03-04 10:48:12 +0100

    [site/theme,site/config][s]: set footer image logo to life itself and tweak title.

--- site/config/siteConfig.js
+++ site/config/siteConfig.js
@@ -1,12 +1,12 @@
 const siteConfig = {
-  title: 'Making Sense of Web3 & Crypto',
+  title: 'Making Sense of Web3',
   tagline: '',
   description: 'Making sense of web3 & crypto. Introduction to key concepts and ideas. Rigorous, constructive analysis of key claims pro and con. A look at the deeper hopes and aspirations.',
   author: 'Life Itself and friends',
   // logo image
-  authorLogo: 'https://playbook.datopian.com/img/datopian-dark-logotype.svg',
+  authorLogo: '/img//life-itself-logo.svg',
   // url to author
-  authorUrl: 'https://datopian.com/',
+  authorUrl: 'https://lifeitself.us/',
   // Google analytics key e.g. G-XXXX
   analytics: '',
   // optional additional nextSeo content set on each page
