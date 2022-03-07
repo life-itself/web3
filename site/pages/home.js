171a5b2598bafe3dbfa876dabb9480000952fecf
Rufus Pollock <rufus.pollock@datopian.com>
2022-03-07 23:13:01 +0100

    [site/home][s]: add section on latest releases with 3 latest youtube episodes.

--- site/pages/home.js
+++ site/pages/home.js
@@ -1,5 +1,35 @@
 export default function Home(props) {
+  const posts = [
+  {
+    title: 'Crypto, Traders and Unfettered Financial Markets',
+    href: 'https://www.youtube.com/watch?v=K5JtPTyc0y0',
+    category: { name: 'Video', href: '#' },
+    description: 'In this episode we discuss the view that crypto are just risky assets and unfettered and unregulated financial markets are desirable.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+  },
+  {
+    title: 'On the Potential of DAOs & Web3 with Jordan Hall',
+    href: 'https://www.youtube.com/watch?v=aQvpgYW0o7M',
+    category: { name: 'Video', href: '#' },
+    description: 'In this episode with Jordan Hall we discuss the potential of DAOs and Web3 for social change.',
+    datetime: '2022-03-01',
+    imageUrl:
+      'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+  },
+  {
+    title: 'Web3, Bitcoin & Neo-Metallism',
+    href: 'https://www.youtube.com/watch?v=_o7pRFLzJHY',
+    category: { name: 'Video', href: '#' },
+    description: 'In this episode we examine Bitcoin and the Neo-Metallist thesis i.e. that a gold-standard was a good idea and a Bitcoin-standard would be even better.',
+    datetime: '2022-02-17',
+    imageUrl:
+      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+  },
+]
+
   return (
+  <>
   <div className="pt-10 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
     <div className="mx-auto max-w-7xl lg:px-8">
       <div className="lg:grid lg:grid-cols-2 lg:gap-8">
@@ -51,6 +81,43 @@ export default function Home(props) {
       </div>
     </div>
   </div>
+
+  <div className="relative pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+    <div className="absolute inset-0">
+      <div className="h-1/3 sm:h-2/3" />
+    </div>
+    <div className="relative max-w-7xl mx-auto">
+      <div className="text-center">
+        <h2 className="text-3xl tracking-tight font-extrabold text-white sm:text-4xl">Latest Releases</h2>
+        <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+          Our latest articles and explorations.
+        </p>
+      </div>
+      <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        {posts.map((post) => (
+          <div key={post.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+            <div className="flex-shrink-0">
+              <img className="h-48 w-full object-cover" src={post.imageUrl} alt="" />
+            </div>
+            <div className="flex-1 bg-slate-800 p-6 flex flex-col justify-between">
+              <div className="flex-1">
+                <p className="text-sm font-medium text-indigo-600 dark:text-yellow-500">
+                  <a href={post.category.href} className="hover:underline">
+                    {post.category.name}
+                  </a>
+                </p>
+                <a href={post.href} className="block mt-2">
+                  <p className="text-xl font-semibold text-slate-900 dark:text-slate-300">{post.title}</p>
+                  <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+  </>
   )
 }
 
