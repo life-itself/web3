b14c18d881ca98862f3a9a0b927d0c7834445a91
Rufus Pollock <rufus.pollock@datopian.com>
2022-03-11 11:59:29 +0100

    [site,bugfix][xs]: fix typo in js in last commit that removed ].

--- site/config/navLinks.js
+++ site/config/navLinks.js
@@ -2,5 +2,6 @@ const navLinks = [
   { href: '/about', name: 'About' },
   { href: 'https://www.zotero.org/groups/4600269/web3/library', name: 'Bibliography' },
   { href: '/claims', name: 'Claims' },
+]
 
 export default navLinks
