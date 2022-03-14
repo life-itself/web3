1ad86d430014a7b8ba987f017f6f112af939dcda
Rufus Pollock <rufus.pollock@datopian.com>
2022-03-14 17:44:06 +0100

    [site/home][xs]: final fix to form id => name attribute.

--- site/components/Home/Hero.js
+++ site/components/Home/Hero.js
@@ -20,7 +20,7 @@ export function Hero() {
                         Email address
                       </label>
                       <input
-                        id="email"
+                        name="email"
                         type="email"
                         placeholder="Enter your email"
                         className="block w-full px-4 py-3 rounded-md border-0 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-gray-900"
