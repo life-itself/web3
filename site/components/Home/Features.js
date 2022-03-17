0b111d9424f460f9bc03fa06783f8901be2ce71f
Rufus Pollock <rufus.pollock@datopian.com>
2022-03-17 15:32:56 +0100

    [site/home][s]: add claims section to front page.

--- site/components/Home/Features.js
+++ site/components/Home/Features.js
@@ -67,6 +67,39 @@ export default function Features() {
           </div>
         </div>
       </div>
+      <div className="mt-24">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+            <div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-slate-100">
+                Claims for Crypto and Web3
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                Crypto and Web3 are associated with many bold claims. From classic technology boosterism of better and faster, to the radical transformation of our societies. But huge controversy surrounds these claims, and there’s a lack of agreement on even basic points and definitions.
+                </p>
+                <div className="mt-6">
+                  <a
+                    href="/claims"
+                    className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-yellow-500 hover:bg-yellow-400"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0">
+            <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                src="https://res.cloudinary.com/ds7qslkd0/image/upload/v1646745400/Web3/undraw_instant_analysis_re_mid5_i8r1hp.svg"
+                alt="Learn More"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
