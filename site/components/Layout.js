fbbbfdbf2c2f8b1296991f9bfbb463643bed94ec
Rufus Pollock <rufus.pollock@datopian.com>
2022-03-17 15:15:54 +0100

    [site/theme][s]: add footer with main links and social media links.

--- site/components/Layout.js
+++ site/components/Layout.js
@@ -4,6 +4,7 @@ import { NextSeo } from 'next-seo'
 
 import Nav from './Nav'
 import siteConfig from '../config/siteConfig'
+import navLinks from '../config/navLinks.js'
 
 export default function Layout({ children, title='' }) {
   return (
@@ -24,18 +25,39 @@ export default function Layout({ children, title='' }) {
       <main>
         {children}
       </main>
-      <footer className="flex items-center justify-center w-full h-24 mt-16">
-        <p className="flex items-center justify-center">
-          Created by
-          <a
-            href={siteConfig.authorUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={siteConfig.authorLogo} alt={siteConfig.author} className="mx-1 h-6 inline-block" />
-            {siteConfig.author}
-          </a>
-        </p>
+      <footer className="w-full h-24 mt-16">
+        <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+          <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+            {navLinks.map((item) => (
+              <div key={item.name} className="px-5 py-2">
+                <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
+                  {item.name}
+                </a>
+              </div>
+            ))}
+          </nav>
+          <div className="mt-8 flex justify-center space-x-6">
+            {siteConfig.social.map((item) => (
+              <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+          <p className="flex items-center justify-center mt-8">
+            Created by
+            <a
+              href={siteConfig.authorUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={siteConfig.authorLogo} alt={siteConfig.author} className="mx-2 h-6 inline-block" />
+              {siteConfig.author}
+              {' '}
+              Licensed under a CC-By 4.0 International License
+            </a>
+          </p>
+        </div>
       </footer>
     </>
   )
