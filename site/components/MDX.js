2e3c65926f6b2b07a82aadd57ce705ef8cd32f51
AceTheCreator <devaze007@gmail.com>
2022-03-30 14:48:05 +0100

    added mdx heading id overwrite

--- site/components/MDX.js
+++ site/components/MDX.js
@@ -1,12 +1,47 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import ContentLayout from "./ContentLayout";
 import { Paragraph } from "./Link";
 import { getPostBySlug, getAllPosts } from '../lib/api';
 
+const CustomHeading = ({ as, ...rest }) => {
+  console.log(as)
+  const [value, setValue] = useState("");
+  const { asPath } = useRouter();
+  const post = getPostBySlug(asPath);
+  const toc = post.toc
+  useEffect(() => {
+    toc.map((t) => {
+      if (t.content === rest.children) {
+        console.log(t.slug)
+        setValue(t.slug);
+      }
+    });
+  }, []);
+  if (as === "h2") {
+    return <h2 id={value} {...rest} />;
+  } else if (as === "h3") {
+    return <h2 id={value} {...rest} />;
+  } else if (as === "h4") {
+    return <h4 id={value} {...rest} />;
+  } else if (as === "h5") {
+    return <h5 id={value} {...rest} />;
+  } else if (as === "h6") {
+    return <h6 id={value} {...rest} />;
+  }
+  return <h1 id={value} {...rest} />;
+};
+
 const components = {
   Head,
   p: Paragraph,
+  h1: (props) => <CustomHeading as="h1" {...props} />,
+  h2: (props) => <CustomHeading as="h2" {...props} />,
+  h3: (props) => <CustomHeading as="h3" {...props} />,
+  h4: (props) => <CustomHeading as="h4" {...props} />,
+  h5: (props) => <CustomHeading as="h5" {...props} />,
+  h6: (props) => <CustomHeading as="h6" {...props} />,
 };
 
 export default function MdxPage({ children }) {
