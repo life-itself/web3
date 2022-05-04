c163f91d132e597fe3ae51f21f89458a1b56ed67
Aleksandra Rubaj <ola@yway.pl>
2022-05-04 17:37:23 +0200

    fix: removed duplicate code with img

--- site/components/Home/Why.js
+++ site/components/Home/Why.js
@@ -127,15 +127,6 @@ export default function Why() {
             </div>
           </div>
         </div>
-        <div className="mt-12 sm:mt-16 lg:mt-0">
-          <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img
-              className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-md"
-              src="/img/undraw_team_collaboration_re_ow29_s8gvrf.svg"
-              alt="Read More"
-            />
-          </div>
-        </div>
       </div>
     </div>
   );
