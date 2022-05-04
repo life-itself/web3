dd1b1c78ed13f586a953cf73bffdf9fee57435c7
Aleksandra Rubaj <ola@yway.pl>
2022-05-04 17:26:21 +0200

    fix: removed negative margin from hero image

--- site/components/Home/Hero.js
+++ site/components/Home/Hero.js
@@ -57,7 +57,7 @@ export function Hero() {
               </p>
             </div>
           </div>
-          <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+          <div className="mt-12 lg:m-0 lg:relative">
             <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
               <img
                 className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
