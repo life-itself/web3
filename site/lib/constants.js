3b1e0350d6d77a38562742938b02e48e8e8e142a
khalilcodes <khalilali.j@gmail.com>
2022-05-18 18:54:57 +0300

    [site][xs]: create file to store regex constants

---
+++ site/lib/constants.js
@@ -0,0 +1,3 @@
+export const YOUTUBE_REGEX = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+
+export const TWITTER_REGEX = /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(es)?\/(\d+)/;
