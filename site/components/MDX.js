2ea5347d2e604f4752f72c538ee16f3500fb0dfe
olayway <ola@yway.pl>
2022-05-18 21:29:00 +0200

    [components/MDX][s]: class attribute fix

--- site/components/MDX.js
+++ site/components/MDX.js
@@ -130,7 +130,7 @@ export default function MdxPage({ children, editUrl }) {
             <a className="flex no-underline font-semibold text-yellow-li" href={editUrl} target="_blank">
               Edit this page
               <span className="mx-1">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                 </svg>
               </span>
