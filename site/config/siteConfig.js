e79aa43ae820aafb9771d336e2bf621b9410a60e
olayway <ola@yway.pl>
2022-05-22 20:18:35 +0200

    [site/config][s]: removed unnecesary property

--- site/config/siteConfig.js
+++ site/config/siteConfig.js
@@ -5,7 +5,6 @@ const siteConfig = {
   url: "https://web3.lifeitself.us",
   repoRoot: "https://github.com/life-itself/web3",
   repoEditPath: "/edit/main/",
-  repoRawContentRoot: "https://raw.githubusercontent.com/life-itself/web3/main",
   tagline: "",
   description:
   "Introductions to key concepts and ideas in crypto and web3. Plus in-depth evaluation of its potential impact.",
