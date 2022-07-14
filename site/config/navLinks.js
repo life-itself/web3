28e1d91a9bc4dd92766269fadf74d8cbbab82bd8
Rufus Pollock <rufus.pollock@datopian.com>
2022-07-14 07:47:56 +0200

    [site/theme,#140][s]: navbar notes link goes to /all.
    * remove pages/notes.js as we have /all page and current notes page is similar to front page now.

--- site/config/navLinks.js
+++ site/config/navLinks.js
@@ -3,7 +3,7 @@ const navLinks = [
   { href: '/guide', name: 'Guide' },
   { href: '/claims', name: 'Claims' },
   { href: '/library', name: 'Library' },
-  { href: '/notes', name: 'Notes' },
+  { href: '/all', name: 'Notes' },
   { href: '/webinars', name: 'Webinars' },
   { href: '/contribute', name: 'Contribute' },
 ]
